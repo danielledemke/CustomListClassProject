5823e2ad80d8d459c440297a8a3f92a108c37fab
Danielle Demke <danielledemke@gmail.com>
2020-02-24 14:46:17 -0600

    Finished adding all unit testing for Add function and got checked by instructor. Working on writing logic for each step in the Add function.

--- CustomListTest/Add_Tests.cs
+++ CustomListTest/Add_Tests.cs
@@ -4,48 +4,106 @@ using PraseodymiumTDD;
 namespace CustomListTest
 {
     [TestClass]
-    public class UnitTest1
+    public class Add_Tests
     {
         [TestMethod]
         public void Add_OneValue_CountIncrement()
         {
             //Arrange
             CustomList<int> myList = new CustomList<int>();
+            int value1 = 5;
             int expected = 1;
+            int actual;
+
 
             //Act
-            myList.Add(5);
+            myList.Add(value1);
+            actual = myList.Count;
 
             //Assert
-            Assert.AreEqual(expected, myList.Count);
+            Assert.AreEqual(expected, actual);
         }
         [TestMethod]
-        public void TestMethod2()
+        public void Add_OneItemAdded_ItemAddsToFirstIndex()
         {
             //Arrange
             CustomList<int> myList = new CustomList<int>();
             int expected = 5;
+            int actual;
 
             //Act
             myList.Add(expected);
+            actual = myList[0];
 
             //Assert
-            Assert.AreEqual(expected, myList[0]);
+            Assert.AreEqual(expected, actual);
         }
         [TestMethod]
-        public void TestMethod3()
+        public void Add_MultipleItemsAdded_ItemAddsToCorrectIndex()
         {
             //Arrange
             CustomList<int> myList = new CustomList<int>();
             int expected = 10;
+            int actual;
             int value1 = 5;
             int value2 = 10;
             //Act
             myList.Add(value1);
             myList.Add(value2);
+            actual = myList[1];
 
             //Assert
-            Assert.AreEqual(expected, myList[1]);
+            Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Add_NumberOfItemsOverCapacity_ItemsCopyToNewArray()
+        {
+            //Arrange
+            CustomList<int> myList = new CustomList<int>();
+            int expected = 12;
+            int actual;
+            int number = 1;
+            int number2 = 2;
+            int number3 = 3;
+            int number4 = 4;
+            //Act
+            myList.Add(number);
+            myList.Add(number2);
+            myList.Add(number3);
+            myList.Add(number4);
+            myList.Add(expected);
+            actual = myList[4];
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        //add 5, what is capacity?
+        [TestMethod]
+        public void Add_OverCapacityLimit_CapacityDoubles()
+        {
+            //arrange
+            CustomList<int> myList = new CustomList<int>();
+            int number = 1;
+            int number2 = 2;
+            int number3 = 3;
+            int number4 = 4;
+            int number5 = 5;
+            int expected = 8;
+            int actual;
+             //act
+            myList.Add(number);
+            myList.Add(number2);
+            myList.Add(number3);
+            myList.Add(number4);
+            myList.Add(number5);
+            actual = myList.Capacity;
+            
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+            
+           
+
     }
 }
--- PraseodymiumTDD/CustomList.cs
+++ PraseodymiumTDD/CustomList.cs
@@ -8,16 +8,85 @@ namespace PraseodymiumTDD
 {
     public class CustomList<T>
     {
+        private T[] array;
+        private int count;
+        private int capacity = 4;
+        
+        public T this[int number]
+        {
+            get
+            {
+                if(number < 0 || number >= array.Length)
+                {
+                    Console.WriteLine("Index out of range");
+                }
+                return T[number];
+            }
+            set
+            {
+                if (number < 0 || number >= array.Length)
+                {
+                    Console.WriteLine("Index out of range");
+                }
+                T[number] = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+        public int Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+        }
+        public CustomList()
+        {
+            array = new T[capacity];
+        }
 
         public void Add(T item)
         {
-            //increment count
+            IncrementCount(); //increment list by one
 
-            //item needs to land at next available index
+            InsertAtAvailableIndex(item); //item needs to land at next available index
 
             //check to make sure it persists
 
             //count == maxcapacity, we need to increase capacity and copy
+        }
+        public void IncrementCount()
+        {
+            count++;
+        }
+
+        public void InsertAtAvailableIndex(T item)
+        {              
+          array[count] = item;
+            
+        }
+
+        public void IncreaseCapacity()
+        {
+            if(count > capacity)
+            {
+                capacity *= 2;
+                T[] newArray = new T[capacity]();
+                newArray = array;
+
+            }
+            
+            
+
+
+
+
         }
     }
 }
